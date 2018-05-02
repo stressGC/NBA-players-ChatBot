d3f6699a6a1ef9e8102414dcead6f5fd16e186ae
Georges Cosson <i352964@dx1sd01hh3qf.dhcp.par2.sap.corp>
2018-05-02 11:40:05 +0200

    added json containing bot data

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
 
-function HandleError (req, res) {
+function HandleError(req, res) {
 	console.log("-------------")
 	console.log("/error called");
 	console.error(req.body);
 	res.sendStatus(200);
 }
 
-function getPlayerStatistics (req, res) {
+function getPlayerStatistics(req, res) {
 	console.log("-------------")
 	console.log("/player-stats called");
 	console.log(req.body);
 }
 
-function getPlayerInformations (req, res) {
+function getPlayerInformations(req, res) {
 	console.log("-------------")
 	console.log("/player-infos called");	
 	console.log(req.body);
