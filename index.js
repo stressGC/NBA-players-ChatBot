4e734ec6538e93b63975e5dd6ee90f0330348a42
Georges Cosson <i352964@dx1sd01hh3qf.dhcp.par2.sap.corp>
2018-05-02 14:18:52 +0200

    add getPlayerByName

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
 var db = JSON.parse(fs.readFileSync('dreamTeam.json', 'utf8'));
+var players = db.players;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,20 @@ app.use(bodyParser.json());
 app.post('/player-stats', getPlayerStatistics);
 app.post('/player-info', getPlayerInformations);
 app.post('/errors', HandleError);
+
+var name = "Chris Mullin";
+console.log(getPlayerByName(name));
+
+
+function getPlayerByName (name) {
+	var player = null
+	players.forEach((element) => {
+		if(element.name == name) {
+			player = element;
+		}
+	});
+	return player;
+}
 
 // Start server
 const PORT = process.env.PORT || 5000;
@@ -24,9 +39,35 @@ function HandleError(req, res) {
 }
 
 function getPlayerStatistics(req, res) {
+
 	console.log("-------------")
 	console.log("/player-stats called");
 	console.log(req.body);
+
+	var player = getPlayerByName(req.body.nlp.entities.nbaplayer[0]);
+	var replyMessage = "Here are the statistics about " + player.name + ".";
+
+
+	var replies = {
+		replies : [{
+			type: 'picture',
+			content: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Michael_Jordan_in_2014.jpg/250px-Michael_Jordan_in_2014.jpg',
+		},
+		{
+			type: "text",
+			content: "- " + player.experience + " years of experience."
+		},
+		{
+			type: "text",
+			content: "- " + player.shotPercent + "% of shot scoring."
+		},		
+		{
+			type: "text",
+			content: "- " + player.PER + " PER."
+		}]
+	}
+
+	res.send(replies);
 }
 
 function getPlayerInformations(req, res) {
