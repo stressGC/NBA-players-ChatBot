ad50cfea1f0ff050f36bdded2da84d7e1bb06ec3
Georges Cosson <i352964@dx1sd01hh3qf.dhcp.par2.sap.corp>
2018-05-02 11:14:46 +0200

    structure error + player stats + player info

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//const db = require('./pokedex.json');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Load routes
+app.post('/player-stats', getPlayerStatistics);
+app.post('/player-info', getPlayerInformations);
+app.post('/errors', HandleError);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+
+
+function HandleError (req, res) {
+	console.log("-------------")
+	console.log("/error called");
+	console.error(req.body);
+	res.sendStatus(200);
+}
+
+function getPlayerStatistics (req, res) {
+	console.log("-------------")
+	console.log("/player-stats called");
+	console.log(req.body);
+}
+
+function getPlayerInformations (req, res) {
+	console.log("-------------")
+	console.log("/player-infos called");	
+	console.log(req.body);
+}
